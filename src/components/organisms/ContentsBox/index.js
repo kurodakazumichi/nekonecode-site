63b6ee0aef6f30bc01367629f570d73af727153c
黒田一道 <>
2019-04-18 06:45:36 +0900

    Added ContentsBoxコンポーネント

---
+++ src/components/organisms/ContentsBox/index.js
@@ -0,0 +1,34 @@
+import React from "react"
+import "./style.scss"
+export default class ContentsBox extends React.Component {
+  static defaultProps = {
+    img: "",
+    title: "",
+    description: "",
+    date: "",
+  }
+  render() {
+    const { img, title, description, date } = this.props
+    return (
+      <div className="o-contentsBox">
+        <div className="kv">
+          <img className="image" src={img} />
+        </div>
+        <div className="info">
+          <span className="title">{title}</span>
+          {this.Description}
+        </div>
+
+        <div className="date">{date}</div>
+      </div>
+    )
+  }
+
+  get Description() {
+    if (!this.props.description) {
+      return null
+    }
+
+    return <p className="desc">{this.props.description}</p>
+  }
+}
