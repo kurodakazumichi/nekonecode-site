c88aa1b11c9959d1426b6e48ff2c911b595627d3
黒田一道 <>
2019-04-18 07:07:55 +0900

    feat サイトタイトルをgraphqlから取得できるようにした。

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  siteMetadata: {
+    title: `Nekonecode`,
+  },
   plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`],
 }
--- src/components/layouts/Standard/index.js
+++ src/components/layouts/Standard/index.js
@@ -1,25 +1,40 @@
 import React from "react"
-import { Link } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 import { Helmet } from "react-helmet"
 import Header from "../../organisms/Header"
 import "./style.scss"
+
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
 export default class extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <Helmet>
-          <link
-            href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c:400,700"
-            rel="stylesheet"
-          />
-        </Helmet>
-        <div className="l-standard">
-          <Header title="Nekonecode" />
-          <main>
-            <div className="container">{this.props.children}</div>
-          </main>
-        </div>
-      </React.Fragment>
+      <StaticQuery
+        query={query}
+        render={data => (
+          <React.Fragment>
+            <Helmet>
+              <link
+                href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c:400,700"
+                rel="stylesheet"
+              />
+            </Helmet>
+            <div className="l-standard">
+              <Header title={data.site.siteMetadata.title} />
+              <main>
+                <div className="container">{this.props.children}</div>
+              </main>
+            </div>
+          </React.Fragment>
+        )}
+      />
     )
   }
 }
