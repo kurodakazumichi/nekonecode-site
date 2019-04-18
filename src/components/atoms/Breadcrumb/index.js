0f3dcdd00c4117dec4bff3e690e5335039c59a84
黒田一道 <>
2019-04-19 05:38:42 +0900

    added Breadcrumbコンポーネントを追加

---
+++ src/components/atoms/Breadcrumb/index.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { Link } from "gatsby"
+import Icon from "../../atoms/Icon"
+import "./style.scss"
+export default class BreadCrumb extends React.Component {
+  static defaultProps = {
+    links: [],
+  }
+
+  render() {
+    return <ul className="a-breadcrumb">{this.Item(this.props.links)}</ul>
+  }
+
+  Item(links) {
+    const lastIndex = links.length - 1
+
+    return links.map((link, index) => {
+      return (
+        <React.Fragment>
+          <li key={index}>
+            <Link to={link.to}>{link.caption}</Link>
+          </li>
+          {index !== lastIndex && (
+            <li>
+              <Icon type="angle-double-right" />
+            </li>
+          )}
+        </React.Fragment>
+      )
+    })
+  }
+}
