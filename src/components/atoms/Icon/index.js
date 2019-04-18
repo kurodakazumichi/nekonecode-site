924f7b281bcab494fe6e99583135dd33b2c3d797
黒田一道 <>
2019-04-19 05:38:24 +0900

    added Iconコンポーネント

---
+++ src/components/atoms/Icon/index.js
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * import area
+ *****************************************************************************/
+import React from "react"
+import ClassNames from "classnames"
+import "@fortawesome/fontawesome-free/css/all.css"
+
+/******************************************************************************
+ *  Enum
+ *****************************************************************************/
+/** Iconのスタイル */
+export const StyleType = {
+  /** 黒塗り */
+  Solid: "fas",
+  /** 中抜き */
+  Regular: "far",
+  /** ブランド */
+  Brand: "fab",
+}
+
+/******************************************************************************
+ * Icon Component
+ *****************************************************************************/
+export default class Icon extends React.Component {
+  /** props規定値 */
+  static defaultProps = {
+    /** アイコンのスタイル */
+    style: StyleType.Solid,
+    type: "info",
+    addClass: "",
+  }
+
+  /** コンストラクタ */
+  constructor(props) {
+    super(props)
+  }
+
+  /** 描画 */
+  render() {
+    return <i className={this.className} />
+  }
+
+  /** CSSクラス名 */
+  get className() {
+    return ClassNames(
+      "icon",
+      this.props.style,
+      `fa-${this.props.type}`,
+      this.props.addClass
+    )
+  }
+}
