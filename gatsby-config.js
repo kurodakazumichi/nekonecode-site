a32f88b72c70da12f64cc243a038b49a03c659ba
黒田一道 <>
2019-04-23 09:46:10 +0900

    added feature that show code title.

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-code-titles`,
           `gatsby-remark-prismjs`,
         ],
       },
